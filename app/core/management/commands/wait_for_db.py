10fe842e6891d1efe400ad73e3cdba635c72d56a
Amir <amir_sameh@yahoo.com>
2024-06-05 23:53:33 +0300

    Migration

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -17,11 +17,18 @@ class Command(BaseCommand):
         """Entrypoint for command."""
         self.stdout.write('Waiting for database...')
         db_up = False
+        max_tries = 10
         while db_up is False:
+            max_tries -= 1
             try:
+                if max_tries < 0 :
+                    raise TimeoutError("Fail to connect with database.")
                 self.check(databases=['default'])
                 db_up = True
             except (Psycopg2Error, OperationalError):
                 self.stdout.write('Database unavailable, watiting 1 second...')
                 time.sleep(1)
+            except RuntimeError as e:
+                self.stdout.write(e)
+
         self.stdout.write(self.style.SUCCESS("Database Avaialble!"))
