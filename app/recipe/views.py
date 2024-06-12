dda7fcd11e055b750a7c97bd4bdb80864eb3d24c
Amir <amir_sameh@yahoo.com>
2024-06-12 18:35:54 +0300

    Recipe attributes

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -5,12 +5,13 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Recipe, Tag
+from core.models import Recipe, Tag, Ingredient
 
 from recipe.serializers import (
     RecipeSerializer,
     RecipeDetailSerializer,
     TagSerializer,
+    IngredientSerializer,
 )
 
 
@@ -36,17 +37,28 @@ class RecipeViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
 
-class TagViewSet(
+class BaseRecipeAttrViewSet(
     mixins.ListModelMixin,
     mixins.UpdateModelMixin,
     mixins.DestroyModelMixin,
     viewsets.GenericViewSet,
 ):
-    """Manage tags in the database."""
-    serializer_class = TagSerializer
-    queryset = Tag.objects.all()
+    """Base viewset for recipe attributes."""
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
+        """Filter queryset to authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by('-name')
+
+
+class TagViewSet(BaseRecipeAttrViewSet):
+    """Manage tags in the database."""
+    serializer_class = TagSerializer
+    queryset = Tag.objects.all()
+
+
+class IngredientViewSet(BaseRecipeAttrViewSet):
+    """Manage ingreidnets in database."""
+    serializer_class = IngredientSerializer
+    queryset = Ingredient.objects.all()
